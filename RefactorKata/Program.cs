2d32bd934d6fae8c8b5cf22f75bd3bd7abc4a7eb
danderson0851 <danderson0851@gmail.com>
2018-01-23 12:45:53 -0600

    Refactored reader variable

--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace RefactorKata
@@ -12,10 +12,8 @@ namespace RefactorKata
 
             var cmd = conn.CreateCommand();
             cmd.CommandText = "select * from Products";
-            /*
-             * cmd.CommandText = "Select * from Invoices";
-             */
-            System.Data.SqlClient.SqlDataReader reader = cmd.ExecuteReader();
+            
+            var reader = cmd.ExecuteReader();
             List<Product> products = new List<Product>();
 
             //TODO: Replace with Dapper
